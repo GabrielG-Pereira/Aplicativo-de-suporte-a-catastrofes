02cc9c2b10c81f280c0d7714e67811f00661aa55
davi1403 <davi.santos01010@gmail.com>
2026-06-06 16:38:19 -0300

    fix: arrumei pequenos detalhes da paginas

--- Soteria.Voluntario/Models/Evento.cs
+++ Soteria.Voluntario/Models/Evento.cs
@@ -5,6 +5,7 @@ namespace Soteria.Voluntario.Models
         public int        Id         { get; set; }
         public string     Titulo     { get; set; } = string.Empty;
         public string     Descricao  { get; set; } = string.Empty;
+        public string     Especialidade { get; set; } = string.Empty;
         public Coordenada Local      { get; set; }
         public Status     Status     { get; set; }
         public DateTime   DataInicio { get; set; }
