0ba9ea850c73a64f10a8b45b7fd1a7bd7fe73a32
davi1403 <davi.santos01010@gmail.com>
2026-06-07 11:22:24 -0300

    feat: pagina doador

--- Soteria.Voluntario/Platforms/Windows/App.xaml.cs
+++ Soteria.Voluntario/Platforms/Windows/App.xaml.cs
@@ -17,6 +17,18 @@ namespace Soteria.Voluntario.WinUI
         public App()
         {
             this.InitializeComponent();
+            Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+            {
+                var nativeWindow = handler.PlatformView;
+                nativeWindow.Activate();
+
+                IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+                var windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+                var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+
+                // Define o tamanho: 450 de largura por 800 de altura (estilo celular)
+                appWindow.Resize(new Windows.Graphics.SizeInt32(450, 800));
+            });
         }
 
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
