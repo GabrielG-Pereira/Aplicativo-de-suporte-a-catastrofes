7e071f13d84ce2f22bfd04dd8f09ac0898355e48
davi1403 <davi.santos01010@gmail.com>
2026-06-10 17:19:28 -0300

    feat: funcoes demanda

---
+++ Soteria.Sql/Models/DemandaLista.cs
@@ -0,0 +1,10 @@
+﻿namespace Soteria.Sql.Models
+{
+    public class DemandaLista
+    {
+        public Guid Id { get; set; }
+        public string CategoriaNome { get; set; } = string.Empty;
+        public string Descricao { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+    }
+}
